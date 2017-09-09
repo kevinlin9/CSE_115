0ff3980edcd54c31375aeb194c1d2d543a1f0b22
Kevin Lin <khlin@buffalo.edu>
2017-09-09 00:06:02 -0400

    Added java file that summarizes the last two week of classwork.

---
+++ Basics/src/code/Demo.java
@@ -0,0 +1,17 @@
+package code;
+
+public class Demo {
+	
+	public static int total(int howMuch, int quantity) {
+		return howMuch * quantity;
+	}
+	public static void receipt(int total) {
+		System.out.println("Your total price is $" + total + ".");
+	}
+	public static void main(String[] args) {
+		int apples = 10;
+		int price = 1;
+		receipt(total(price, apples));
+	}
+	
+}
