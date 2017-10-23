5ce5488dcbc3739a7d49c4f57c506533718ffe3d
Kevin Lin <khlin@buffalo.edu>
2017-10-23 15:50:20 -0400

    10/23/17 classwork

--- Basics/src/code/GUI_Demo.java
+++ Basics/src/code/GUI_Demo.java
@@ -1,20 +1,36 @@
 package code;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 public class GUI_Demo {
 	public static void main(String[] args) {
 		String title = "This is a JFrame window";
 		JFrame mainWindow = new JFrame(title);
-		mainWindow.setVisible(true);
-		int width = 1440;
+		/*int width = 1440;
 		int height = 700;
-		mainWindow.setSize(width, height);
+		mainWindow.setSize(width, height);*/
 		String text = "This is a label with a whole bunch of text on it. The quick brown fox jumped over the big blue wall.";
 		JLabel label = new JLabel(text);
 		label.setHorizontalAlignment(SwingConstants.CENTER);
-		mainWindow.add(label);
+		
+		JPanel mainPanel = new JPanel();
+		mainWindow.add(mainPanel);
+		mainPanel.setLayout(new FlowLayout());
+		for(int i = 0; i < 20; i++) {
+			JLabel l = new JLabel("Label #" + i);
+			mainPanel.add(l);
+		}
+		mainPanel.add(label);
+		JButton button = new JButton("Clickable button");
+		mainPanel.add(button);
+		
+		mainWindow.pack();
+		mainWindow.setVisible(true);
 	}
 }
